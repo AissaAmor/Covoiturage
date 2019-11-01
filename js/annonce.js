e0243c9d6bed88294019316c8382f1d37f2541ee
AissaAmor <amor.aissa1993@gmail.com>
2019-11-01 11:52:28 +0100

    modifier profil

--- js/annonce.js
+++ js/annonce.js
@@ -14,6 +14,7 @@ function checkradio3() {
 }
 function addTrajet() {
     var mytable = JSON.parse(localStorage.getItem('table'));
+    var connectedUser = JSON.parse(localStorage.getItem("User"));
     var dep = document.getElementById('depart1').value;
     var dest = document.getElementById('arrive').value;
     var date = document.getElementById('dateDepart').value;
@@ -32,7 +33,7 @@ function addTrajet() {
         musique: document.querySelector('input[name="music"]:checked').value,
         commentaire: comm,
         id: ran
-    }
+        }
     if (mytable == null) {
         mytable = []
     }
@@ -41,23 +42,25 @@ function addTrajet() {
 }
 
 function afficheAnnonceProfil() {
-    // document.getElementById('part1').style.display = 'block';
-    // document.getElementById('part2').style.display = 'none';
+    document.getElementById('part2').style.display = 'none';
+    document.getElementById('part1').style.display = 'block';
     var mytable = JSON.parse(localStorage.getItem('table'));
     var html = ``;
     for (i = 0; i < mytable.length; i++) {
         html += `<tr>
-                        <td>Lieu de depart :${mytable[i].departTrajet}<br> Destination :${mytable[i].arriveTrajet}
-                            <br>Date de trajet :${mytable[i].dateTrajet}<br> Prix par passager :${mytable[i].prix}
-                            <br>Nombre de places : ${mytable[i].place}<br> Climatisation : ${mytable[i].climatisation}
-                            <br> Tabac : ${mytable[i].fumees}<br> Musique : ${mytable[i].musique}
+                        <td id="bt1"><label id="titre2">Trajet N°${i+1}</label><br>
+                            <label id="attribut">Lieu de depart :${mytable[i].departTrajet}</label><br>
+                            <label id="attribut">Destination :${mytable[i].arriveTrajet}</label><br>
+                            <label id="attribut">Date de trajet :${mytable[i].dateTrajet}<br> Prix par passager :${mytable[i].prix}</label><br>
+                            <label id="attribut">Nombre de places : ${mytable[i].place}<br> Climatisation : ${mytable[i].climatisation}</label><br>
+                            <label id="attribut">Tabac : ${mytable[i].fumees}<br> Musique : ${mytable[i].musique}</label>
                         </td>
                         <td id="btt2">
-                            <a href="blog.html"><input type="button" value="Modifier" id="btt1" onclick="editAnnonce(${i},${mytable[i].id})"></a>
+                            <input type="button" value="Modifier" id="btt1" onclick="afficheAnnonce(${i})">
                             <input type="button" value="Delete"id="btt1" onclick='deleteAnnonce(${mytable[i].id})'>
                             </td>
                             </tr>`
-        console.log(mytable[i].id);
+            // console.log(mytable[i].id);
     }
     document.getElementById('table1').innerHTML = html;
 }
@@ -73,10 +76,34 @@ function deleteAnnonce(idAnnonce) {
     console.log(mytable);
     this.afficheAnnonceProfil();
 }
-// document.getElementById('part2').style.display = 'none';
-function editAnnonce(pos, idTask) {
-    // document.getElementById('part1').style.display = 'none';
-    // document.getElementById('part2').style.display = 'block';
+document.getElementById('part2').style.display = 'none';
+function afficheAnnonce(pos) {
+    var html=``
+    html += `<input type="button" value="Valider" onclick="editAnnonce(${pos})">`
+    console.log(pos);
+    document.getElementById('part1').style.display = 'none';
+    document.getElementById('part2').style.display = 'block';
     var mytable = JSON.parse(localStorage.getItem('table'));
+    document.getElementById('cout').value = mytable[pos].prix;
     document.getElementById('nombrePlaces').value = mytable[pos].place;
+    document.getElementById('dateDep').value = mytable[pos].dateTrajet;
+    document.getElementById('btt').innerHTML=html;
+}
+function editAnnonce(pos){
+    var mytable = JSON.parse(localStorage.getItem('table'));
+    console.log(pos);
+    var todo = {
+        prix: document.getElementById('cout').value,
+        placee: document.getElementById('nombrePlaces').value,
+        dateT:document.getElementById('dateDep').value
+        // climatisation: document.querySelector('input[name="clima"]:checked').value,
+        // fumees: document.querySelector('input[name="tabac"]:checked').value,
+        // musique: document.querySelector('input[name="musique"]:checked').value,
+        // commentaire: document.getElementById('comm').value,
+    }
+    
+    mytable.splice(pos,1,todo);
+    localStorage.setItem('table', JSON.stringify(mytable));
+    console.log(mytable);
+    // this.editAnnonce();
 }
